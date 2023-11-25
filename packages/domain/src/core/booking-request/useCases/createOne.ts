998a7a899413733615bc94e428b689a17802a6da
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-11-25 22:21:45 +0100

    fix price calculation

--- packages/domain/src/core/booking-request/useCases/createOne.ts
+++ packages/domain/src/core/booking-request/useCases/createOne.ts
@@ -28,7 +28,6 @@ async function persistMenuBookingRequest(
     menuBookingRequest: CreateOneMenuBookingRequestRequest & { userId: NanoId },
     paymentData: { setupIntentId: string; clientSecret: string },
 ): Promise<boolean> {
-    const fee: number = 22;
     const { dataSourceAdapter, logger } = runtime;
     const {
         userId,
@@ -70,7 +69,7 @@ async function persistMenuBookingRequest(
     );
 
     const totalAmountUser: number = (menuPrice + menuPrice * 0.04 + travelExpensesAmount + 25) / (1 - 0.015);
-    const totalAmountCook: number = Math.trunc((menuPrice * (100 - fee)) / 100) + travelExpensesAmount;
+    const totalAmountCook: number = Math.trunc((menuPrice * (100 - 18)) / 100) + travelExpensesAmount;
 
     const success: boolean = await dataSourceAdapter.bookingRequestRepository.insertOne({
         bookingRequestId,
