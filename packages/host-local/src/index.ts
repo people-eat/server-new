01aa879247799acce2904e8ef29fb5e5b6863d8b
Cem Yilmaz <cem.yilmaz@sap.com>
2024-04-07 12:09:45 +0200

    update web app url for local host

--- packages/host-local/src/index.ts
+++ packages/host-local/src/index.ts
@@ -72,7 +72,7 @@ async function bootstrap(): Promise<void> {
         identityProviderAdapter,
         paymentAdapter,
         serverUrl: 'http://localhost:4000',
-        webAppUrl: 'http://localhost:3000',
+        webAppUrl: 'http://localhost:4200',
         publisher: {
             publish: async (key: string, payload: any) => {
                 await pubsub.publish(key, payload);
